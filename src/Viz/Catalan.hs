3fc2daeb570114b596ad1b52c51cfb4c7519bb5c
Noam Zeilberger <noam.zeilberger@gmail.com>
2016-08-22 09:34:25 +0200

    fix problem with line thickness

--- src/Viz/Catalan.hs
+++ src/Viz/Catalan.hs
@@ -19,37 +19,27 @@ attach n1 n2 n =
             (position [(location b2,fromOffsets [ 100*unitX # rotateBy (5/8)])] :: Path V2 Double)
   in
    atop $
-   (((position [(p,circle 0.2 # fc black # named n & pad 3)]))
-    # connectOutside' (with & arrowHead .~ noHead) n n1
-    # connectOutside' (with & arrowHead .~ noHead) n n2)
+   position [(p,circle 0.2 # lwL 0.1 # fc black # named n & pad 3)]
 
 diagCatTree :: String -> Catalan -> Diagram B
-diagCatTree k L = (circle 0.2 # fc blue & pad 3) # named k
+diagCatTree k L = (circle 0.2 # lwL 0.1 # fc blue & pad 3) # named k
 diagCatTree k (B t1 t2) =
   let d1 = diagCatTree ('L' : k) t1 in
   let d2 = diagCatTree ('R' : k) t2 in
   let k1 = ('L' : k) in
   let k2 = ('R' : k) in
-  withName k1 (\b1 ->
-   withName k2 $ \b2 ->
-   let p = head $ intersectPoints
-           (position [(location b1,fromOffsets [ 100*unitX # rotateBy (7/8)])] :: Path V2 Double)
-           (position [(location b2,fromOffsets [ 100*unitX # rotateBy (5/8)])] :: Path V2 Double)
-   in
-    atop $
-    (position [(p,circle 0.2 # fc black # named k)])) $
   attach k1 k2 k (d1 ||| d2)
-  # connectOutside' (with & arrowHead .~ noHead) k k1
-  # connectOutside' (with & arrowHead .~ noHead) k k2
+  # (connectOutside' (with & arrowHead .~ noHead) k k1 # lwL 0.1)
+  # (connectOutside' (with & arrowHead .~ noHead) k k2 # lwL 0.1)
 
 diagDyckArcs :: [Int] -> Diagram B
 diagDyckArcs w =
-  let pt n = circle 0.2 # fc black # named n in
+  let pt n = circle 0.2 # lwL 0.1 # fc black # named n in
   let marked [] seen = []
       marked (x:xs) seen = if elem x seen then (True,x):marked xs seen else (False,x):marked xs (x:seen) in
   let w' = marked w [] in
   let shaft = arc xDir (-1/2 @@ turn) in
   hsep 1 (map pt w') #
-  applyAll [connect' (with & arrowHead .~ noHead & arrowShaft .~ shaft) (False,i) (True,i) | (False,i) <- w']
+  applyAll [connect' (with & arrowHead .~ noHead & arrowShaft .~ shaft) (False,i) (True,i) # lwL 0.2 | (False,i) <- w']
 
 
--- src/Viz/List.hs
+++ src/Viz/List.hs
@@ -9,10 +9,10 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
 labelledHList :: [(Diagram B,Diagram B)] -> [Diagram B]
-labelledHList xis = map (\(x,i) -> hsep 0.5 [alignB (fromOffsets [unit_Y,unitX] ||| i) & frame 1, alignB x]) xis
+labelledHList xis = map (\(x,i) -> hsep 0.5 [alignB (fromOffsets [unit_Y,unitX] # lwL 0.5 ||| i) & frame 1, alignB x]) xis
 
 labelledVList :: [(Diagram B,Diagram B)] -> [Diagram B]
-labelledVList xis = map (\(x,i) -> hsep 0.5 [alignT (fromOffsets [unitY,unitX] === i) & frame 1, alignT x]) xis
+labelledVList xis = map (\(x,i) -> hsep 0.5 [alignT (fromOffsets [unitY,unitX] # lwL 0.5 === i) & frame 1, alignT x]) xis
 
 numberedHList :: [Diagram B] -> [Diagram B]
 numberedHList xs = labelledHList $ zip xs (map (text . show) [1..])
