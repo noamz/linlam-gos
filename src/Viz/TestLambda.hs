c32cbf0aa64808de564bfb1dc6841cfed8d1bd47
Noam Zeilberger <noam.zeilberger@gmail.com>
2016-08-30 14:05:46 +0200

    with numbering

--- src/Viz/TestLambda.hs
+++ src/Viz/TestLambda.hs
@@ -16,16 +16,16 @@ import qualified Lambda as Lam
 import qualified Chapo as Ch
 
 main = do
-  putStr "n: "
-  n <- getLine >>= return . read
+  putStr "(n,b): "
+  (n,b) <- getLine >>= return . read
   putStrLn "generating svg..."
-  let b = True
   let nptis = Ch.allnpti b n
   let byApps = equivClassesBy (\t1 t2 -> Ch.apps2cat t1 == Ch.apps2cat t2) nptis []
   let byApps' = sortBy (comparing length) $ map (sortBy (\t1 t2 -> let (c1,c2) = (Ch.apps2cat t1,Ch.apps2cat t2) in if c1 == c2 then EQ else if T.tamari_order c1 c2 then LT else if T.tamari_order c2 c1 then GT else compare (Lam.prettyULT t1) (Lam.prettyULT t2))) byApps
-  let byApps'' = map (\ts -> (Ch.apps2cat (head ts),ts)) byApps'
-
+  let numbered = reverse $ snd $ foldl (\(n,tnss) ts -> let (m,tns) = foldl (\(m,tns) t -> (m+1,(t,m):tns)) (n,[]) ts in (m, (reverse tns) : tnss)) (1,[]) byApps'
+  let numbered' = map (\tns -> (Ch.apps2cat (fst $ head tns),tns)) numbered
+  let mkDiagram t = vsep 1 [diagULT b t # centerX, text (Lam.prettyULT t) # centerX ]
   let d = vsep 1 $ labelledVList [(diagCatTree c # scale 2,
-                                   hsep 3 $ labelledHList $ zip (map (\t -> vsep 1 [diagULT b t # centerX, text (Lam.prettyULT t) # centerX ]) ts) (repeat mempty)) | (c,ts) <- byApps'']
+                                   hsep 3 $ labelledHList $ map (\(t,n) -> (mkDiagram t, text (show n))) tns) | (c,tns) <- numbered' ]
 
   mainWith (d # frame 1)
