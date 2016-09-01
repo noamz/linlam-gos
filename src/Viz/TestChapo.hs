1e4325a85f4c1e57a105ba98440c2d8cf44f8f0d
Noam Zeilberger <noam.zeilberger@gmail.com>
2016-09-01 18:59:06 +0200

    propagate renamings

--- src/Viz/TestChapo.hs
+++ src/Viz/TestChapo.hs
@@ -21,11 +21,11 @@ main = do
   n <- getLine >>= return . read
   putStrLn "generating svg..."
   let nptis = Ch.allnptiLR n
-  let byApps = equivClassesBy (\t1 t2 -> Ch.apps2cat t1 == Ch.apps2cat t2) nptis []
-  let byApps' = sortBy (comparing length) $ map (sortBy (\t1 t2 -> let (c1,c2) = (Ch.apps2cat t1,Ch.apps2cat t2) in if c1 == c2 then EQ else if T.tamari_order c1 c2 then LT else if T.tamari_order c2 c1 then GT else compare (Lam.prettyULT t1) (Lam.prettyULT t2))) byApps
-  let byApps'' = map (\ts -> (Ch.apps2cat (head ts),ts)) byApps'
+  let byApps = equivClassesBy (\t1 t2 -> Ch.apps2tree t1 == Ch.apps2tree t2) nptis []
+  let byApps' = sortBy (comparing length) $ map (sortBy (\t1 t2 -> let (c1,c2) = (Ch.apps2tree t1,Ch.apps2tree t2) in if c1 == c2 then EQ else if T.tamari_order c1 c2 then LT else if T.tamari_order c2 c1 then GT else compare (Lam.prettyULT t1) (Lam.prettyULT t2))) byApps
+  let byApps'' = map (\ts -> (Ch.apps2tree (head ts),ts)) byApps'
   
-  let tminterval t = (Ch.lams2dowLR t,Ch.apps2cat t)
-  let d = vsep 1 $ labelledVList [(diagCatTree c # scale 2,
-                                   hsep 3 $ labelledHList $ zip (map (\t -> vsep 1 [diagDyckArcs (Ch.lams2dowLR t) # centerX, text (Lam.prettyULT t) # centerX ]) ts) (repeat mempty)) | (c,ts) <- byApps'']
+  let tminterval t = (Ch.lams2dowLR t,Ch.apps2tree t)
+  let d = vsep 1 $ labelledVList [(diagTree c # scale 2,
+                                   hsep 3 $ labelledHList $ zip (map (\t -> vsep 1 [diagArcs (Ch.lams2dowLR t) # centerX, text (Lam.prettyULT t) # centerX ]) ts) (repeat mempty)) | (c,ts) <- byApps'']
   mainWith (d # frame 1)
--- src/Viz/TestLambda.hs
+++ src/Viz/TestLambda.hs
@@ -20,12 +20,12 @@ main = do
   (n,b) <- getLine >>= return . read
   putStrLn "generating svg..."
   let nptis = Ch.allnpti b n
-  let byApps = equivClassesBy (\t1 t2 -> Ch.apps2cat t1 == Ch.apps2cat t2) nptis []
-  let byApps' = sortBy (comparing length) $ map (sortBy (\t1 t2 -> let (c1,c2) = (Ch.apps2cat t1,Ch.apps2cat t2) in if c1 == c2 then EQ else if T.tamari_order c1 c2 then LT else if T.tamari_order c2 c1 then GT else compare (Lam.prettyULT t1) (Lam.prettyULT t2))) byApps
+  let byApps = equivClassesBy (\t1 t2 -> Ch.apps2tree t1 == Ch.apps2tree t2) nptis []
+  let byApps' = sortBy (comparing length) $ map (sortBy (\t1 t2 -> let (c1,c2) = (Ch.apps2tree t1,Ch.apps2tree t2) in if c1 == c2 then EQ else if T.tamari_order c1 c2 then LT else if T.tamari_order c2 c1 then GT else compare (Lam.prettyULT t1) (Lam.prettyULT t2))) byApps
   let numbered = reverse $ snd $ foldl (\(n,tnss) ts -> let (m,tns) = foldl (\(m,tns) t -> (m+1,(t,m):tns)) (n,[]) ts in (m, (reverse tns) : tnss)) (1,[]) byApps'
-  let numbered' = map (\tns -> (Ch.apps2cat (fst $ head tns),tns)) numbered
+  let numbered' = map (\tns -> (Ch.apps2tree (fst $ head tns),tns)) numbered
   let mkDiagram t = vsep 1 [diagULT b t # centerX, text (Lam.prettyULT t) # centerX ]
-  let d = vsep 1 $ labelledVList [(diagCatTree c # scale 2,
+  let d = vsep 1 $ labelledVList [(diagTree c # scale 2,
                                    hsep 3 $ labelledHList $ map (\(t,n) -> (mkDiagram t, text (show n))) tns) | (c,tns) <- numbered' ]
 
   mainWith (d # frame 1)
