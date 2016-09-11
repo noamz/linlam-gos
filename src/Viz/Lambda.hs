09c8308d132bae273b6694464f7a589e22fb66a8
Noam Zeilberger <noam.zeilberger@gmail.com>
2016-09-11 23:30:24 +0200

    visualization of types

--- src/Viz/Lambda.hs
+++ src/Viz/Lambda.hs
@@ -64,3 +64,53 @@ diagULT b t =
   let w = Ch.lams2arcs b t in
   VC.diagArcs_glue ns (if b then w else C.fliparcs $ reverse w) d
 
+typeTree :: String -> L.Type -> (Diagram B, [String])
+typeTree k (L.TVar _) = (spot # named k, [k])
+typeTree k (L.TFn t1 t2) =
+  let (d1,s1) = typeTree ('L' : k) t1 in
+  let (d2,s2) = typeTree ('R' : k) t2 in
+  let k1 = ('L' : k) in
+  let k2 = ('R' : k) in
+  (attach False k1 k2 k (d1 ||| d2)
+   # (connectOutside' (with & arrowHead .~ noHead) k k1 # lwL 0.1)
+   # (connectOutside' (with & arrowHead .~ noHead) k k2 # lwL 0.1),
+   s1 ++ s2)
+
+posTree :: String -> L.Type -> (Diagram B, [String])
+negTree :: String -> L.Type -> (Diagram B, [String])
+posTree k (L.TVar _) = (spot # named k, [k])
+posTree k (L.TFn t1 t2) =
+  let (d1,s1) = posTree ('L' : k) t2 in
+  let (d2,s2) = negTree ('R' : k) t1 in
+  let k1 = ('L' : k) in
+  let k2 = ('R' : k) in
+  (attach False k1 k2 k (d1 ||| d2)
+   # (connectOutside' (with & arrowHead .~ noHead) k k1 # lwL 0.1)
+   # (connectOutside' (with & arrowHead .~ noHead) k k2 # lwL 0.1),
+   s1 ++ s2)
+negTree k (L.TVar _) = (spot # named k, [k])
+negTree k (L.TFn t1 t2) =
+  let (d1,s1) = posTree ('L' : k) t1 in
+  let (d2,s2) = negTree ('R' : k) t2 in
+  let k1 = ('L' : k) in
+  let k2 = ('R' : k) in
+  (attach True k1 k2 k (d1 ||| d2)
+   # (connectOutside' (with & arrowHead .~ noHead) k k1 # lwL 0.1)
+   # (connectOutside' (with & arrowHead .~ noHead) k k2 # lwL 0.1),
+   s1 ++ s2)
+
+diagPos :: L.Type -> Diagram B
+diagPos t =
+  let (d,ns) = posTree [] t in
+  let w = C.dow2arcs (L.linearizePos t) in
+  VC.diagArcs_glue ns w d
+
+diagType :: Bool -> L.Type -> Diagram B
+diagType False t =
+  let (d,ns) = posTree [] t in
+  let w = C.dow2arcs (L.linearizePos t) in
+  VC.diagArcs_glue ns w d
+diagType True t =
+  let (d,ns) = typeTree [] t in
+  let w = C.dow2arcs (L.linearizeType t) in
+  VC.diagArcs_glue ns w d
---
+++ src/Viz/TestTypes.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+import Diagrams.Prelude
+import Diagrams.Backend.SVG.CmdLine
+
+import Viz.Catalan
+import Viz.List
+import Viz.Lambda
+
+import Data.List
+import Data.Ord
+
+import Bijections
+import qualified Tamari as T
+import qualified Lambda as Lam
+import qualified Chapo as Ch
+
+main = do
+  putStr "(n,b): "
+  (n,b) <- getLine >>= return . read
+  putStrLn "generating svg..."
+  let nptis = Ch.allnpti b n
+  let byApps = equivClassesBy (\t1 t2 -> Ch.apps2tree t1 == Ch.apps2tree t2) nptis []
+  let byApps' = sortBy (comparing length) $ map (sortBy (\t1 t2 -> let (c1,c2) = (Ch.apps2tree t1,Ch.apps2tree t2) in if c1 == c2 then EQ else if T.tamari_order c1 c2 then LT else if T.tamari_order c2 c1 then GT else compare (Lam.prettyULT t1) (Lam.prettyULT t2))) byApps
+  let numbered = reverse $ snd $ foldl (\(n,tnss) ts -> let (m,tns) = foldl (\(m,tns) t -> (m+1,(t,m):tns)) (n,[]) ts in (m, (reverse tns) : tnss)) (1,[]) byApps'
+  let numbered' = map (\tns -> (Ch.apps2tree (fst $ head tns),tns)) numbered
+  let mkDiagram t =
+        let tp = Lam.synthClosedNormal t in
+        hsep 1 [vsep 1 [diagULT b t # centerX, text (Lam.prettyULT t) # centerX ],
+                text ":",
+                vsep 1 [diagType b tp # centerX, text (Lam.prettyType tp) # centerX]]
+  let d = vsep 1 $ labelledVList [(diagTree c # scale 2,
+                                   hsep 3 $ labelledHList $ map (\(t,n) -> (mkDiagram t, text (show n))) tns) | (c,tns) <- numbered' ]
+
+  mainWith (d # frame 1)
