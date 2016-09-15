2a35ca42df4f0bd6a69ccf34247c13c0e6a11d35
Noam Zeilberger <noam.zeilberger@gmail.com>
2016-09-15 11:26:25 +0200

    more combinators

--- src/Combinators.hs
+++ src/Combinators.hs
@@ -10,8 +10,23 @@ cI = L 0 $ V 0
 cB :: ULT
 cB = L 0 $ L 1 $ L 2 $ A (V 0) $ A (V 1) $ V 2
 
+cB' :: ULT
+cB' = L 0 $ L 1 $ L 2 $ A (V 2) $ A (V 1) $ V 0
+
+-- LR-planar term which normalizes to B
+cBLR :: ULT
+cBLR = L 0 $ L 1 $ L 2 $ A (A (A (L 3 $ L 4 $ L 5 $ A (V 5) $ A (V 4) $ V 3) (V 2)) (V 1)) (V 0)
+
 cC :: ULT
 cC = L 0 $ L 1 $ L 2 $ A (A (V 0) (V 2)) (V 1)
+
+-- LR-planar term which normalizes to C
+cCLR :: ULT
+cCLR = L 0 $ L 1 $ L 2 $ A (A (A (L 3 $ L 4 $ A (A (L 5 $ L 6 $ L 7 $ A (A (V 7) (V 6)) (V 5)) (V 4)) (V 3)) (V 2)) (V 1)) (V 0)
+
+
+cDNI :: ULT
+cDNI = L 0 $ L 1 $ A (V 1) (V 0)
 
 cstr :: ULT
 cstr = L 0 $ L 1 $ L 2 $ A (V 0) (L 3 $ A (V 2) $ A (V 3) (V 1))
