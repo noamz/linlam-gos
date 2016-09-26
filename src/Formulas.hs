e6b05bb66f3822dae8dd3dc091e51fe9e0e2e55e
Noam Zeilberger <noam.zeilberger@gmail.com>
2016-09-26 20:51:36 +0200

    fix exponential time complexity bug

--- src/Formulas.hs
+++ src/Formulas.hs
@@ -13,7 +13,7 @@ fact n = if n < 0 then -1 else n * fact (n-1)
 -- binom k n = if k>n then 0 else fact n `div` (fact k * fact (n-k))
 binom 0 n = 1
 binom k 0 = 0
-binom k n = binom (k-1) (n-1) + binom k (n-1)
+binom k n = binom (k-1) (n-1) * n `div` k
 
 catalan n = binom n (2*n) `div` (n+1)
 
