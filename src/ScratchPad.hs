0b3072c0055ac487788ab4c255d3ace67f87913f
Noam Zeilberger <noam.zeilberger@gmail.com>
2016-10-27 14:23:12 +0200

    interesting conjecture about non-separable maps

--- src/ScratchPad.hs
+++ src/ScratchPad.hs
@@ -2,6 +2,7 @@ module ScratchPad where
 
 import Data.List
 
+import Formulas
 import Lambda
 import Maps
 
@@ -22,3 +23,15 @@ test n =
   in
    (sort $ map sort $ map termsig ts) ==
    (sort $ map ((sort . map length) . verticesOM) ms)
+
+-- verified for 1<=n<=9
+test2 n =
+  let ts = allcNPTnb True n in
+  let a000139' n = if n == 0 then 1 else
+                     2*fact (3*n) `div` (fact (2*n+1) * fact(n+1)) in
+  let nsts =
+        [t | t <- ts, flip all (t:subnormal_subnormals t) $ \t' ->
+          let (xs,u) = unlambdas t' in
+          length xs <= 1 + length (snd (unapps u []))] in
+  toInteger (length nsts) == a000139' (toInteger (n-1))
+
